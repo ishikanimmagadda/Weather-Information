2f04b3251765362ecf2fba54d0963769ffdd8f20
ishikanimmagadda <ishikan84@gmail.com>
2024-06-21 15:32:16 -0500

    outputting weather data!!!!

---
+++ weatherdisplay.py
@@ -0,0 +1,31 @@
+import requests
+
+api_key = 'b02c752e95bc925d00ba424a7f1eeebc'
+
+city = input("Enter a city name: ")
+
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+response = requests.get(url)
+
+#successful response 
+if response.status_code == 200: 
+    #dictionary 
+    weatherData = response.json()
+
+    maintemperature = weatherData['main']['temp']
+    maintempCelsius = maintemperature - 273.15
+
+    feelsliketemp = weatherData ['main']['feels_like']
+    feelslikeCelsius = feelsliketemp - 273.15
+
+    windSpeed = weatherData['wind']['speed']
+
+    description = weatherData['weather'][0]['description']
+
+    print("Temperature: " + str(maintempCelsius))
+    print("Feels Like: " + str(feelslikeCelsius))
+    print("Wind Speed: " + str(windSpeed))
+    print("Description: " + str(description))
+else: 
+    print("error")
